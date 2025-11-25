d9f44ab54a7e10d5deba856ec3069c997f820012
Vinco Wu <viwu@konradgroup.com>
2025-11-25 14:08:07 -0500

    updating script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 set -euo pipefail
 
-cd /path/to/DDM-OS-Reminder/Resources
+cd ./Resources
 zsh assembleDDMOSReminder.zsh
 zsh ddmOSReminder.Assembled.*.zsh
 launchctl kickstart -kp system/org.kg.dor 
