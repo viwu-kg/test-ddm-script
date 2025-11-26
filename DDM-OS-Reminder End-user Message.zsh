372a30a45698fdb07f7b1b16a52366a9fc4fab6a
Vinco Wu <viwu@konradgroup.com>
2025-11-26 16:05:36 -0500

    adding icon

--- DDM-OS-Reminder End-user Message.zsh
+++ DDM-OS-Reminder End-user Message.zsh
@@ -256,24 +256,26 @@ function updateRequiredVariables() {
 
 
     # macOS Installer Icon URL (these are generic platform icons; adjust if desired)
-    majorDDM="${ddmVersionString%%.*}"
-    case ${majorDDM} in
-        14)  macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_eecee9688d1bc0426083d427d80c9ad48fa118b71d8d4962061d4de8d45747e7" ;;
-        15)  macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_0968afcd54ff99edd98ec6d9a418a5ab0c851576b687756dc3004ec52bac704e" ;;
-        26)  macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_7320c100c9ca155dc388e143dbc05620907e2d17d6bf74a8fb6d6278ece2c2b4" ;;
-        *)   macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_4555d9dc8fecb4e2678faffa8bdcf43cba110e81950e07a4ce3695ec2d5579ee" ;;
-    esac
+    # majorDDM="${ddmVersionString%%.*}"
+    # case ${majorDDM} in
+    #     14)  macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_eecee9688d1bc0426083d427d80c9ad48fa118b71d8d4962061d4de8d45747e7" ;;
+    #     15)  macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_0968afcd54ff99edd98ec6d9a418a5ab0c851576b687756dc3004ec52bac704e" ;;
+    #     26)  macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_7320c100c9ca155dc388e143dbc05620907e2d17d6bf74a8fb6d6278ece2c2b4" ;;
+    #     *)   macOSIconURL="https://ics.services.jamfcloud.com/icon/hash_4555d9dc8fecb4e2678faffa8bdcf43cba110e81950e07a4ce3695ec2d5579ee" ;;
+    # esac
 
-    # Download the icon from ${macOSIconURL}
-    if [[ -n "${macOSIconURL}" ]]; then
-        curl -o "/var/tmp/icon.png" "${macOSIconURL}" --silent --show-error --fail
-        if [[ "$?" -ne 0 ]]; then
-            error "Failed to download the icon from '${macOSIconURL}'."
-            icon="/System/Library/CoreServices/Finder.app"
-        else
-            icon="./images/kg.png"
-        fi
-    fi
+    # # Download the icon from ${macOSIconURL}
+    # if [[ -n "${macOSIconURL}" ]]; then
+    #     curl -o "/var/tmp/icon.png" "${macOSIconURL}" --silent --show-error --fail
+    #     if [[ "$?" -ne 0 ]]; then
+    #         error "Failed to download the icon from '${macOSIconURL}'."
+    #         icon="/System/Library/CoreServices/Finder.app"
+    #     else
+    #         icon="/var/tmp/icon.png"
+    #     fi
+    # fi
+
+    icon="../images/kg.png"
 
 
 
